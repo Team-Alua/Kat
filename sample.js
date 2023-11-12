c147cfb30d080da02e1c73a1080b49f62102b641
ac2pic <ac2pic@gmail.com>
2023-11-12 11:40:17 -0600

    Added API list

---
+++ sample.js
@@ -0,0 +1,16 @@
+// console.log(...)
+// discord.getMessage()
+// discord.sendMessage("")
+// discord.uploadFile(name, contentType, fh)
+// fs.open(path, flags, perm) => fh
+// fs.mkdir(path)
+// fs.readdir(path) => []string
+// fs.mount(src, dest, {"MountType": string, "ReadOnly": bool}) => mh
+// fs.umount(mh)
+// fs.read(fh, byteAmount)
+// fs.readline(fh) => string
+// fs.write(fh, buffer)
+// fs.writeString(fh, "")
+// fs.copy(w, r)
+// fs.close(fh)
+// download(path, url)
