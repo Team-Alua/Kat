9b0bb21094484f031806bb3924f4ad8f523df4da
ac2pic <ac2pic@gmail.com>
2023-11-18 20:59:45 -0600

    Add StreamReader

--- interpreter/interpreter_stream.go
+++ interpreter/interpreter_stream.go
@@ -1,9 +1,51 @@
 package interpreter
 
 import (
-//	"bufio"
+	"github.com/dop251/goja"
+	"io"
+	"bufio"
 )
 
+
 func (i *Interpreter) LoadStreamBuiltins() {
+	vm := i.vm
+
+	vm.Set("StreamReader", func(call goja.ConstructorCall) *goja.Object {
+		fp, ok := call.Argument(0).Export().(io.Reader)
+		if !ok {
+			panic("First argument must be Readable.")
+		}
+
+		reader := bufio.NewReader(fp)
+
+		call.This.Set("readUntil", func(icall goja.FunctionCall) goja.Value {
+			d := icall.Argument(0).String();
+
+			if len(d) != 1 {
+				panic("First argument must a single character.")
+			}
+			b := []byte(d)
+			buf, err := reader.ReadBytes(b[0])
+			if err != nil {
+				panic(err)
+			}
+			return vm.ToValue(buf)
+		})
+
+		call.This.Set("read", func(icall goja.FunctionCall) goja.Value {
+			// int32
+			c := icall.Argument(0).ToInteger();
+
+			buf := make([]byte, c)
+			n, err := reader.Read(buf);
+			if err != nil {
+				panic(err)
+			}
+			buf = buf[0:n]
+			return vm.ToValue(buf)
+		})
+
+		return nil
+	})
 }
 
